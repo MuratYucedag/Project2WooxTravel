e2c1176810a8f3c88e84bff47660aa9d5003256a
MuratYucedag <yucedagmurat23@gmail.com>
2024-10-15 22:15:26 +0300

    ...

--- Project2WooxTravel/Areas/Admin/Controllers/CategoryController.cs
+++ Project2WooxTravel/Areas/Admin/Controllers/CategoryController.cs
@@ -11,6 +11,8 @@ namespace Project2WooxTravel.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         TravelContext context = new TravelContext();
+
+        [Authorize]
         public ActionResult CategoryList()
         {
             var values = context.Categories.ToList();
---
+++ Project2WooxTravel/Controllers/ErrorPagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2WooxTravel.Controllers
+{
+    public class ErrorPagesController : Controller
+    {
+        // GET: ErrorPages
+        public ActionResult Page404()
+        {
+            return View();
+        }
+    }
+}
